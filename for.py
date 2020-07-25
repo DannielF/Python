ed391e84e49168afdc9f507d6da5b193521ee21d
DannielF <danielgranados1992@gmail.com>
2020-07-25 16:59:40 -0500

    Add for, recorrer

---
+++ for.py
@@ -0,0 +1,14 @@
+# for contador in range(1001):
+#     print(contador)
+# imprimir los numeros del 1 al 1000
+
+# for i in range(10):
+#     print(11 * i)
+# imprimir la multiplicacion de 11 por el rango seleccionado    
+
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador += 1
+#     print(contador) 
+# Como mostrar los numeros del 1 al 100
