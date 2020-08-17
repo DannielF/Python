4d5df2fcbd8ec9834d315a4b6ce81d20e91a774a
DannielF <danielgranaods1992@gmail.com>
2020-08-17 12:33:07 -0500

    Add variance /standard deviation

--- statistic.py
+++ statistic.py
@@ -1,13 +1,27 @@
-#sacar la media con codigo
 import random
+import math
 
-
-def media(X): 
+def media(X): #sacar la media
     return sum(X) / len(X)
+
+def variance(X):
+    mu = media(X)
+
+    accumulator = 0 #guardar resultados sumatoria
+    for x in X:
+        accumulator += (x - mu)**2
+    return round(accumulator / len(X), 4)
+
+def standard_deviation(X):
+    return round(math.sqrt (variance(X)), 4) #regresar raiz cuadrada varianza    
 
 if __name__ == "__main__":
     X = [random.randint(1, 20) for i in range(20)] #valores 1 al 20/ generar 20 valores
     µ = media(X) #mu(µ) media poblacion
+    Var = variance(X)  #Var de varianza
+    σ = standard_deviation(X) #σ simbolo desviacion estandar
 
-    print(X)
-    print(f'it´s media is: {µ}')
+    print(f'The list is: {X}')
+    print(f'Media is: {µ}')
+    print(f'Variance is: {Var}')
+    print(f'Standard deviation is: {σ}')
